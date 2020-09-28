cf6797ee148a08451a9922462c832f749895ba8f
TomRobette <robettetom@gmail.com>
2020-09-28 09:37:59 +0000

    SHARE-0.3

--- src/Form/AjoutUtilisateurType.php
+++ src/Form/AjoutUtilisateurType.php
@@ -18,7 +18,7 @@ class AjoutUtilisateurType extends AbstractType
             ->add('nom', TextType::class)
             ->add('prenom', TextType::class)
             ->add('datenaissance', DateType::class, [
-                'years' => range(date('Y')-500, date('Y')),
+                'years' => range(date('Y')-1500, date('Y')),
             ])
             ->add('dateinscription', DateType::class)      
             ->add('send', SubmitType::class)
